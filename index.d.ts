dfb2e036ef4d74bda8e71c3caa94648f63d352d4
Pawel <jarrodek@gmail.com>
2020-05-14 21:01:45 -0700

    chore: adding Utils export through index.js

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,5 @@
+import * as Utils from './src/Utils';
+
 export { ApiFormMixin } from './src/ApiFormMixin';
 export { default as apiFormStyles } from './src/ApiFormStyles';
+export { Utils };
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+import * as Utils from './src/Utils.js';
+
 export { ApiFormMixin } from './src/ApiFormMixin.js';
 export { default as apiFormStyles } from './src/ApiFormStyles.js';
+export { Utils };
